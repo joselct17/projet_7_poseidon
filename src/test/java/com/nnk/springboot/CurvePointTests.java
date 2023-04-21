cf06a69064c9f742b3c3708c5c288143539a74e9
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-21 10:11:13 +0200

    rajout du test testGetBidList

--- src/test/java/com/nnk/springboot/CurvePointTests.java
+++ src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -1,23 +1,39 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.controllers.CurveController;
+import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.repositories.CurvePointRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CurvePointTests {
 
-	@Autowired
+	@Mock
 	private CurvePointRepository curvePointRepository;
+
+	@Mock
+	private Model model;
+	@InjectMocks
+	private CurveController curveController;
 
 	@Test
 	public void curvePointTest() {
@@ -42,6 +58,20 @@ public class CurvePointTests {
 		curvePointRepository.delete(curvePoint);
 		Optional<CurvePoint> curvePointList = curvePointRepository.findById(id);
 		Assert.assertFalse(curvePointList.isPresent());
+	}
+
+
+	@Test
+	public void testGetBidList() {
+		// Arrange
+		when(curvePointRepository.findAll()).thenReturn(new ArrayList<CurvePoint>());
+
+		// Act
+		String viewName = curveController.home(model);
+
+		// Assert
+		verify(model, times(1)).addAttribute(eq("curvePoints"), any());
+		assertEquals("curvePoint/list", viewName);
 	}
 
 }
