3896284d8391a22819a2c222e662f9298b5269a2
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-21 10:11:37 +0200

    rajout du test testGetBidList

--- src/test/java/com/nnk/springboot/RuleTests.java
+++ src/test/java/com/nnk/springboot/RuleTests.java
@@ -1,23 +1,42 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.controllers.RatingController;
+import com.nnk.springboot.controllers.RuleNameController;
+import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RatingRepository;
 import com.nnk.springboot.repositories.RuleNameRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RuleTests {
 
-	@Autowired
+	@Mock
 	private RuleNameRepository ruleNameRepository;
+
+	@Mock
+	private Model model;
+
+	@InjectMocks
+	private RuleNameController ruleNameController;
 
 	@Test
 	public void ruleTest() {
@@ -42,5 +61,19 @@ public class RuleTests {
 		ruleNameRepository.delete(rule);
 		Optional<RuleName> ruleList = ruleNameRepository.findById(id);
 		Assert.assertFalse(ruleList.isPresent());
+	}
+
+
+	@Test
+	public void testGetBidList() {
+		// Arrange
+		when(ruleNameRepository.findAll()).thenReturn(new ArrayList<RuleName>());
+
+		// Act
+		String viewName = ruleNameController.home(model);
+
+		// Assert
+		verify(model, times(1)).addAttribute(eq("ruleNames"), any());
+		assertEquals("ruleName/list", viewName);
 	}
 }
