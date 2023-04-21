9741c57ba58b300dd986e14f8ad00de561be0778
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-21 10:11:49 +0200

    rajout du test testGetBidList

--- src/test/java/com/nnk/springboot/TradeTests.java
+++ src/test/java/com/nnk/springboot/TradeTests.java
@@ -1,23 +1,42 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.controllers.RuleNameController;
+import com.nnk.springboot.controllers.TradeController;
+import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.repositories.RuleNameRepository;
 import com.nnk.springboot.repositories.TradeRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TradeTests {
 
-	@Autowired
+	@Mock
 	private TradeRepository tradeRepository;
+
+	@Mock
+	private Model model;
+
+	@InjectMocks
+	private TradeController tradeController;
 
 	@Test
 	public void tradeTest() {
@@ -42,5 +61,18 @@ public class TradeTests {
 		tradeRepository.delete(trade);
 		Optional<Trade> tradeList = tradeRepository.findById(id);
 		Assert.assertFalse(tradeList.isPresent());
+	}
+
+	@Test
+	public void testGetBidList() {
+		// Arrange
+		when(tradeRepository.findAll()).thenReturn(new ArrayList<Trade>());
+
+		// Act
+		String viewName = tradeController.home(model);
+
+		// Assert
+		verify(model, times(1)).addAttribute(eq("trades"), any());
+		assertEquals("trade/list", viewName);
 	}
 }
