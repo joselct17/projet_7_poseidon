35be7420bba7fdcfdbbebd45d75727fe02dfe46e
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-21 10:11:25 +0200

    rajout du test testGetBidList

--- src/test/java/com/nnk/springboot/RatingTests.java
+++ src/test/java/com/nnk/springboot/RatingTests.java
@@ -1,23 +1,43 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.controllers.CurveController;
+import com.nnk.springboot.controllers.RatingController;
+import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.CurvePointRepository;
 import com.nnk.springboot.repositories.RatingRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RatingTests {
 
-	@Autowired
+
+
+	@Mock
 	private RatingRepository ratingRepository;
+
+	@Mock
+	private Model model;
+	@InjectMocks
+	private RatingController ratingController;
 
 	@Test
 	public void ratingTest() {
@@ -42,5 +62,18 @@ public class RatingTests {
 		ratingRepository.delete(rating);
 		Optional<Rating> ratingList = ratingRepository.findById(id);
 		Assert.assertFalse(ratingList.isPresent());
+	}
+
+	@Test
+	public void testGetBidList() {
+		// Arrange
+		when(ratingRepository.findAll()).thenReturn(new ArrayList<Rating>());
+
+		// Act
+		String viewName = ratingController.home(model);
+
+		// Assert
+		verify(model, times(1)).addAttribute(eq("ratings"), any());
+		assertEquals("rating/list", viewName);
 	}
 }
