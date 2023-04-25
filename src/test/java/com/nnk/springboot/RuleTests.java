9cd50edb340ecad8563f23ea0fc9e92ea4873394
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-25 17:47:10 +0200

    creation et implementation des test testPostRule_Success(), testGetUpdateForm(), testPostUpdateFormAndRedirect_SUCCESS(), updateRuleName_shouldReturnUpdateViewWhenBindingResultHasErrors()

--- src/test/java/com/nnk/springboot/RuleTests.java
+++ src/test/java/com/nnk/springboot/RuleTests.java
@@ -7,10 +7,12 @@ import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.repositories.RatingRepository;
 import com.nnk.springboot.repositories.RuleNameRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +20,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +55,14 @@ public class RuleTests {
 
 	@InjectMocks
 	private RuleNameController ruleNameController;
+
+	@Mock
+	private BindingResult bindingResult;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(ruleNameController).build();
+	}
 
 	@Test
 	public void ruleTest() {
@@ -109,5 +123,80 @@ public class RuleTests {
 
 		// Assert that the view name is "/ruleName/add"
 		assertEquals("ruleName/add", result.getModelAndView().getViewName());
+	}
+
+	@Test
+	public void testPostRule_Success() throws Exception {
+
+		// Arrange
+		RuleName ruleName = new RuleName();
+
+		ruleName.setId(1);
+		ruleName.setName("Name test");
+		ruleName.setJson("Json test");
+		ruleName.setTemplate("Template test");
+		ruleName.setSqlStr("SqlStr test");
+
+		//Act
+		mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/validate")
+						.flashAttr("ruleName", ruleName))
+				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+				.andExpect(MockMvcResultMatchers.redirectedUrl("/ruleName/list"));
+
+		//Assert
+		Mockito.verify(ruleNameRepositoryMock, Mockito.times(1)).save(ruleName);
+		Mockito.verify(ruleNameRepositoryMock, Mockito.times(1)).findAll();
+
+	}
+
+	@Test
+	public void testGetUpdateForm() throws Exception {
+		// Arrange
+		RuleName ruleName = new RuleName();
+		ruleName.setId(1);
+		Mockito.when(ruleNameRepositoryMock.findById(1)).thenReturn(Optional.of(ruleName));
+
+		// Act
+		mockMvc.perform(MockMvcRequestBuilders.get("/ruleName/update/{id}", 1))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("ruleName/update"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("ruleName"))
+				.andExpect(MockMvcResultMatchers.model().attribute("ruleName", ruleName));
+
+		// Assert
+		Mockito.verify(ruleNameRepositoryMock, Mockito.times(1)).findById(1);
+	}
+
+	@Test
+	public void testPostUpdateFormAndRedirect_SUCCESS() {
+		// Arrange
+		Integer id = 1;
+		RuleName ruleName = new RuleName();
+
+		when(bindingResult.hasErrors()).thenReturn(false);
+		when(ruleNameRepositoryMock.save(ruleName)).thenReturn(null);
+
+		// Act
+		String result = ruleNameController.updateRuleName(id, ruleName, bindingResult, model);
+
+		// Assert
+		verify(ruleNameRepositoryMock).save(ruleName);
+		verify(model).addAttribute(eq("ruleNames"), anyList());
+		assertEquals("redirect:/ruleName/list", result);
+	}
+
+	@Test
+	public void updateRuleName_shouldReturnUpdateViewWhenBindingResultHasErrors() {
+		// Arrange
+		Integer id = 1;
+		RuleName ruleName = new RuleName();
+		when(bindingResult.hasErrors()).thenReturn(true);
+
+		// Act
+		String result = ruleNameController.updateRuleName(id, ruleName, bindingResult, model);
+
+		// Assert
+		verifyNoInteractions(ruleNameRepositoryMock);
+		assertEquals("ruleName/update", result);
 	}
 }
