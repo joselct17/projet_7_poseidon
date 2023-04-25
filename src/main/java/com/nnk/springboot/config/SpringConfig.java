3615d4d0d562ec4e758aeefde0bd564f02845999
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-25 12:06:58 +0200

    model mapper rajout

---
+++ src/main/java/com/nnk/springboot/config/SpringConfig.java
@@ -0,0 +1,17 @@
+package com.nnk.springboot.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    //This is for mapping Entity-DTO
+    //https://www.baeldung.com/entity-to-and-from-dto-for-a-java-spring-application
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
