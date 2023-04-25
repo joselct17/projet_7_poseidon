96e84ff1688b95a28c248020453348cd161a8de0
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-25 15:38:01 +0200

    implementation du test pour avoir le formulaire add

--- src/test/java/com/nnk/springboot/CurvePointTests.java
+++ src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -10,26 +10,38 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.ui.Model;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.Double.valueOf;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 public class CurvePointTests {
 
 	@Mock
+	private CurvePointRepository curvePointRepositoryMock;
+	@Autowired
 	private CurvePointRepository curvePointRepository;
 
+	@Autowired
+	private MockMvc mockMvc;
 	@Mock
 	private Model model;
 	@InjectMocks
@@ -37,7 +49,7 @@ public class CurvePointTests {
 
 	@Test
 	public void curvePointTest() {
-		CurvePoint curvePoint = new CurvePoint();
+		CurvePoint curvePoint = new CurvePoint( 1, 10, new Timestamp(20), valueOf(20), valueOf(30), new Timestamp(20) );
 
 		// Save
 		curvePoint = curvePointRepository.save(curvePoint);
@@ -62,9 +74,9 @@ public class CurvePointTests {
 
 
 	@Test
-	public void testGetBidList() {
+	public void testCurvePointList() {
 		// Arrange
-		when(curvePointRepository.findAll()).thenReturn(new ArrayList<CurvePoint>());
+		when(curvePointRepositoryMock.findAll()).thenReturn(new ArrayList<CurvePoint>());
 
 		// Act
 		String viewName = curveController.home(model);
@@ -73,5 +85,26 @@ public class CurvePointTests {
 		verify(model, times(1)).addAttribute(eq("curvePoints"), any());
 		assertEquals("curvePoint/list", viewName);
 	}
+
+	@Test
+	public void testAddCurvePoint() throws Exception {
+		// Create a mock curvePoint object
+		CurvePoint curve = new CurvePoint();
+		curve.setId(1);
+		curve.setTerm(valueOf(20));
+		curve.setCurveId(1);
+		curve.setValue(valueOf(20));
+
+		// Perform a GET request to the /curvePoint/add endpoint and pass the mock bid object as a parameter
+		MvcResult result = mockMvc.perform(get("/curvePoint/add").flashAttr("curve", curve)).andReturn();
+
+		// Assert that the response status is OK
+		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+
+		// Assert that the view name is "/curvePoint/add"
+		assertEquals("curvePoint/add", result.getModelAndView().getViewName());
+	}
+
+
 
 }
