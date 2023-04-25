1a7117e02673ea96461e07dffb6c4fb18d87822a
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-25 17:44:09 +0200

    correction des noms des requettes

--- src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -37,7 +37,7 @@ public class CurveController {
     }
 
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addCurveForm(CurvePoint bid) {
         logger.info("GET/curvePoint/add");
         return "curvePoint/add";
     }
@@ -65,7 +65,7 @@ public class CurveController {
     }
 
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Validated CurvePoint curvePoint,
+    public String updateCurve(@PathVariable("id") Integer id, @Validated CurvePoint curvePoint,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
         if (result.hasErrors()) {
@@ -80,7 +80,7 @@ public class CurveController {
     }
 
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteCurve(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
         CurvePoint curvePoint = curvePointRepository.findById(id).orElseThrow(() ->new IllegalArgumentException("Invalid item id:" + id));
         curvePointRepository.delete(curvePoint);
