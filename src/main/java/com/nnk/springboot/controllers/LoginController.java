0675003aeb8ce0ab20133900d6018ea07a37ada5
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-25 12:05:43 +0200

    modification du controller j'ai enlevé le app

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("app")
 public class LoginController {
 
     Logger logger = LoggerFactory.getLogger(LoginController.class);
