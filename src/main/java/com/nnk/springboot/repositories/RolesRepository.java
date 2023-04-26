97eae2361df7c62158b39aa9b850bdb668073696
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-26 16:08:41 +0200

    elimination

--- src/main/java/com/nnk/springboot/repositories/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nnk.springboot.repositories;
-
-import com.nnk.springboot.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<Role, Integer> {
-}
